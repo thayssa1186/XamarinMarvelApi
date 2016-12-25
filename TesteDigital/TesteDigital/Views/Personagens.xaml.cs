c47cc15dc2b4f57db213991288ec0ef428dd46b3
thaissa <thayssa1186@outlook.com.br>
2016-12-25 13:34:36 -0200

    versão 3

--- TesteDigital/TesteDigital/Views/Personagens.xaml.cs
+++ TesteDigital/TesteDigital/Views/Personagens.xaml.cs
@@ -19,20 +19,7 @@ namespace TesteDigital
 
         protected async override void OnAppearing()
         {
-            base.OnAppearing();
-
-            if (Constants.RestUrl.Contains("developer.xamarin.com"))
-            {
-                if (!alertShown)
-                {
-                    await DisplayAlert(
-                        "Hosted Back-End",
-                        "This app is running against Xamarin's read-only REST service. To create, edit, and delete data you must update the service endpoint to point to your own hosted REST service.",
-                        "OK");
-                    alertShown = true;
-                }
-            }
-
+            base.OnAppearing();                       
             listView.ItemsSource = await App.TesteDigitalManager.GetTasksAsync();
         }
 
